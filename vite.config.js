d6cdef86270e9f0a77380861092946f2a60fbe99
snoodd <127285199+snoodd@users.noreply.github.com>
2026-07-30 16:34:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // relative asset paths — works whether hosted at a domain root or a subpath like /repo-name/
+  base: "./",
 });
